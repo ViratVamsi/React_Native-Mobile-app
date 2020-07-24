783260f21f01928ed64c8d57700bcfe5275f882f
vamsi <virat.vamsi58@gmail.com>
2020-07-24 13:16:38 +0530

    Navigation Part 3

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,7 +1,36 @@
 import React,{Component} from 'react';
-import {Text,View} from 'react-native';
+import { Text, ScrollView, View } from 'react-native';
+import { Card } from 'react-native-elements';
+import { DISHES } from '../shared/dishes';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
+
+function RenderItem(props){
+    return(
+        <Card
+            featuredTitle={props.item.name}
+            featuredSubtitle={props.item.designation}
+            image={require('../images/uthappizza.png')}
+            >
+                <Text
+                    style={{margin:10}}
+                    >
+                    {props.item.description}
+                </Text>
+        </Card>
+    );
+}
 
 class Home extends Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          dishes: DISHES,
+          promotions: PROMOTIONS,
+          leaders: LEADERS
+        };
+    }
 
     static navigationOptions = {
         title: 'Home'
@@ -9,7 +38,11 @@ class Home extends Component{
 
     render() {
         return(
-            <View><Text>Home Component</Text></View>
+            <ScrollView>
+                <RenderItem item={this.state.dishes.filter((dish)=>dish.featured)[0]} />
+                <RenderItem item={this.state.promotions.filter((promo)=>promo.featured)[0]} />
+                <RenderItem item={this.state.leaders.filter((leader)=>leader.featured)[0]} />
+            </ScrollView>
         );
     }
 }
